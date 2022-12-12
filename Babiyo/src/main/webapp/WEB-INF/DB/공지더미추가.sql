76b1ed3565d5ecca586b2ca7054b8c7299151913
EZEN-144 <EZEN-144@DESKTOP-8H1JNTH>
2022-12-12 17:50:13 +0900

    팀장: 이동현
    더미파일 추가

---
+++ Babiyo/src/main/webapp/WEB-INF/DB/공지더미추가.sql
@@ -0,0 +1,355 @@
+SELECT
+    * FROM member;
+--더미 데이터
+--문의
+desc inquiry;
+
+insert into member
+(id, password, name, email, birthday, gender, phone, nickname, cash
+, grade_code, create_date, modify_date)
+values('test', '123', '테스트', 'test@test.com', TO_DATE('19960305')
+    , '여', '01055553303', '테스트', 100000, 2, sysdate, sysdate);
+
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 1, '테스트', '테스트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUEs('dong', 1, '테스트', '테스트', SYSDATE);
+
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 2, '밀키트 관련', '밀키트', SYSDATE);
+
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('test', 3, '회원', '회원문의', SYSDATE);
+
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+
+ 
+INSERT INTO inquiry
+VALUE(member_id, value.category_code, value.title, value.content, value.create_date)
+VALUES('dong', 4, '기타문의', '기타', SYSDATE);
+
+UPDATE inquiry
+set
+answer ='답변테스트'
+WHERE no BETWEEN 10 and 30;
+
+--공지
+desc notice;
+
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date)
+VALUES(1, '공지', '공지테스트', SYSDATE);
+
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '이벤트', '이벤트테스트', SYSDATE, SYSDATE, SYSDATE + 10);
+
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+INSERT INTO notice
+VALUE(value.category_code, value.title, value.content, value.create_date ,value.event_start_date, value.event_end_date)
+VALUES(2, '종료', '종료된이벤트', SYSDATE, SYSDATE-30, SYSDATE - 20);
+
+SELECT * 
+FROM notice;
+--랭킹
+
+
+commit;
